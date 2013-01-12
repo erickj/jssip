3c724a5d2c8b992ac46bbc95c76512ce6def2c28
Erick Johnson <erick@ejjohnson.org>
2013-01-12 17:01:41 -0500

    made tokenmatcher an interface and added RegexpTokenMatcher and AnyTokenMatcher

--- spec/util/tokenmatcher_spec.js
+++ spec/util/tokenmatcher_spec.js
@@ -1,33 +1,49 @@
 goog.provide('jssip.util.TokenMatcherSpec');
 
-goog.require('jssip.util.TokenMatcher');
+goog.require('jssip.util.RegexpTokenMatcher');
+goog.require('jssip.util.AnyTokenMatcher');
 
 describe('jssip.util.TokenMatcher', function() {
-  var pattern = /^a$/;
-  var matchingToken = 'a';
-  var nonmatchingToken = 'b';
+  describe('jssip.util.RegexpTokenMatcher', function() {
+    var pattern = /^a$/;
+    var matchingToken = 'a';
+    var nonmatchingToken = 'b';
 
-  var tokenMatcher;
-  var antiTokenMatcher;
+    var tokenMatcher;
+    var antiTokenMatcher;
 
-  beforeEach(function() {
-    tokenMatcher = new jssip.util.TokenMatcher(pattern);
-    antiTokenMatcher = new jssip.util.TokenMatcher(pattern, true);
+    beforeEach(function() {
+      tokenMatcher = new jssip.util.RegexpTokenMatcher(pattern);
+      antiTokenMatcher = new jssip.util.RegexpTokenMatcher(pattern, true);
+    });
+
+    it('should accept if there is a match', function() {
+      expect(tokenMatcher.test(matchingToken)).toBe(true);
+    });
+
+    it('should reject if there is not a match', function() {
+      expect(tokenMatcher.test(nonmatchingToken)).toBe(false);
+    });
+
+    it('should accept a reject match if there is no match', function() {
+      expect(antiTokenMatcher.test(nonmatchingToken)).toBe(true);
+    });
+
+    it('should reject a match when testing for reject match', function() {
+      expect(antiTokenMatcher.test(matchingToken)).toBe(false);
+    });
   });
 
-  it('should accept if there is a match', function() {
-    expect(tokenMatcher.test(matchingToken)).toBe(true);
-  });
-
-  it('should reject if there is not a match', function() {
-    expect(tokenMatcher.test(nonmatchingToken)).toBe(false);
-  });
-
-  it('should accept a reject match if there is no match', function() {
-    expect(antiTokenMatcher.test(nonmatchingToken)).toBe(true);
-  });
-
-  it('should reject a match when testing for reject match', function() {
-    expect(antiTokenMatcher.test(matchingToken)).toBe(false);
+  describe('jssip.util.AnyTokenMatcher', function() {
+    it('should return true any token matcher matches or false otherwise',
+       function() {
+         var tokenMatcherA = new jssip.util.RegexpTokenMatcher(/a/);
+         var tokenMatcherB = new jssip.util.RegexpTokenMatcher(/b/);
+         var anyTokenMatcher = new jssip.util.AnyTokenMatcher(
+             [tokenMatcherA, tokenMatcherB]);
+         expect(anyTokenMatcher.test('a')).toBe(true);
+         expect(anyTokenMatcher.test('b')).toBe(true);
+         expect(anyTokenMatcher.test('c')).toBe(false);
+       });
   });
 });
--- src/util/tokenmatcher.js
+++ src/util/tokenmatcher.js
@@ -1,15 +1,34 @@
 goog.provide('jssip.util.TokenMatcher');
+goog.provide('jssip.util.RegexpTokenMatcher');
+goog.provide('jssip.util.AnyTokenMatcher');
 
 
 /**
- * Provides an object for matching token patterns positively or
- * negatively.
+ * The token matcher interface provides a test method to determine if
+ * a string token is a match for some condition.
+ * @interface
+ */
+jssip.util.TokenMatcher = function() {};
+
+
+/**
+ * @param {string} token The token to test.
+ * @return {boolean} Whether the token matches.
+ */
+jssip.util.TokenMatcher.prototype.test = function(token) {};
+
+
+
+/**
+ * An object for matching token patterns positively or negatively
+ * against regular expressions.
  * @param {!RegExp} regex The regular expression to match.
  * @param {boolean=} opt_rejectMatch Whether the test should fail if the
  *     pattern matches.  Default is false.
+ * @implements {jssip.util.TokenMatcher}
  * @constructor
  */
-jssip.util.TokenMatcher = function(regex, opt_rejectMatch) {
+jssip.util.RegexpTokenMatcher = function(regex, opt_rejectMatch) {
   /**
    * @type {!RegExp}
    * @private
@@ -24,11 +43,36 @@ jssip.util.TokenMatcher = function(regex, opt_rejectMatch) {
 };
 
 
-/**
- * @param {string} token The token to test.
- * @return {boolean} Whether the token matches.
- */
-jssip.util.TokenMatcher.prototype.test = function(token) {
+/** @override */
+jssip.util.RegexpTokenMatcher.prototype.test = function(token) {
   var match = this.regex_.test(token);
   return this.rejectMatch_ ? !match : match;
+};
+
+
+
+/**
+ * A token matcher that will return true if any of the token matchers
+ * supplied in the constructor return a positive match.
+ * @param {!Array<!jssip.util.TokenMatcher>} matchers The matchers array.
+ * @implements {jssip.util.TokenMatcher}
+ * @constructor
+ */
+jssip.util.AnyTokenMatcher = function(matchers) {
+  /**
+   * @type {!Array.<!jssip.util.TokenMatcher>}
+   * @private
+   */
+  this.matchers_ = matchers;
+};
+
+
+/** @override */
+jssip.util.AnyTokenMatcher.prototype.test = function(token) {
+  for (var i = 0; i < this.matchers_.length; i++) {
+    if (this.matchers_[i].test(token)) {
+      return true;
+    }
+  }
+  return false;
 };
