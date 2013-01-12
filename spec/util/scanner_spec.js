a09c3e7505c8f7baf3f9fc556f79d44a37210f0f
Erick Johnson <erick@ejjohnson.org>
2013-01-11 23:28:29 -0500

    scanner and scanner spec

---
+++ spec/util/scanner_spec.js
@@ -0,0 +1,56 @@
+goog.provide('jssip.util.ScannerSpec');
+
+goog.require('jssip.util.Scanner');
+
+describe('jssip.util.Scanner', function() {
+  it('should return the next token position', function() {
+    var str = "the rain in spain\r\nfalls mainly on the\r\nplane";
+    var scanner = new jssip.util.Scanner(str);
+
+    // first position, 17
+    expect(scanner.nextTokenPosition(jssip.util.Scanner.TOKEN.CRLF)).toBe(17);
+    expect(scanner.getPosition()).toBe(17 + jssip.util.Scanner.TOKEN.CRLF.length);
+
+    // second position, 38
+    expect(scanner.nextTokenPosition(jssip.util.Scanner.TOKEN.CRLF)).toBe(38);
+    expect(scanner.getPosition()).toBe(38 + jssip.util.Scanner.TOKEN.CRLF.length);
+
+    // there is no third token, -1
+    expect(scanner.nextTokenPosition(jssip.util.Scanner.TOKEN.CRLF)).toBe(-1);
+    expect(scanner.getPosition()).toBe(-1);
+  });
+
+  it('should throw an error if there is nothing left to scan', function() {
+    var scanner = new jssip.util.Scanner('foobar');
+    scanner.nextTokenPosition(['z']);
+    expect(function() { scanner.nextTokenPosition(['z']) }).toThrow();
+  });
+
+  it('can take an array of chars as a token', function() {
+    var str = "foobar";
+    var scanner = new jssip.util.Scanner(str);
+
+    expect(scanner.nextTokenPosition(['o','b'])).toBe(2);
+  });
+
+  it('should return substrings of message text', function() {
+    var scanner = new jssip.util.Scanner('foobar');
+    expect(scanner.getSubstring(2,4)).toBe('ob');
+
+    expect(scanner.getSubstring(2,20)).toBe('obar');
+  });
+
+  it('should throw an error if substring start is greater than end',
+     function() {
+       var scanner = new jssip.util.Scanner('foobar');
+       expect(function() { scanner.getSubstring(4,2) }).toThrow();
+     });
+
+  it('should return true from isEof when the whole string is scanned',
+     function() {
+       var scanner = new jssip.util.Scanner('foobar');
+       expect(scanner.isEof()).toBe(false);
+       scanner.nextTokenPosition(['z']);
+       expect(scanner.isEof()).toBe(true);
+     });
+});
---
+++ src/util/scanner.js
@@ -0,0 +1,104 @@
+goog.provide('jssip.util.Scanner');
+
+/**
+ * A string scanner.
+ * @param {string} text The text to scan.
+ * @constructor
+ */
+jssip.util.Scanner = function(text) {
+  /**
+   * @type {string}
+   * @private
+   */
+  this.text_ = text;
+
+  /**
+   * The current location. -1 indicates EOF.
+   * @type {number}
+   * @private
+   */
+  this.index_ = 0;
+};
+
+
+/**
+ * @type {number}
+ * @const
+ */
+jssip.util.Scanner.EOF = -1;
+
+
+/**
+ * @type {enum}
+ */
+jssip.util.Scanner.TOKEN = {
+  CRLF: ['\r', '\n']
+};
+
+
+/**
+ * Returns the current position of the scanner
+ * @return {number} The current position.
+ */
+jssip.util.Scanner.prototype.getPosition = function() {
+  return this.index_;
+};
+
+
+/**
+ * @return {boolean} Whether the scanner has reached the end of the string.
+ */
+jssip.util.Scanner.prototype.isEof = function() {
+  return this.index_ == jssip.util.Scanner.EOF;
+};
+
+
+/**
+ * Gets the substring from the message from position start to end. If end is
+ * greater than message length return from start to the end of the message.  If
+ * start is greater than end, throw an error.
+ * @param {number} start The start position.
+ * @param {number} end The end position.
+ * @return {string} The substring.
+ */
+jssip.util.Scanner.prototype.getSubstring = function(start, end) {
+  if (start > end) {
+    throw new Error('Start index must be less than end index');
+  }
+  return this.text_.substring(start, end);
+};
+
+
+/**
+ * Return the start position of the next token in the text or -1 if
+ * no such occurrence.  The position is advanced to the end of the
+ * token or EOF if no match is found. Throws an error if scanning has
+ * previously reached EOF.
+ * @param {jssip.util.Scanner.TOKEN|Array<string>} token The token.
+ * @return {number} The position of the first character in the token.
+ */
+jssip.util.Scanner.prototype.nextTokenPosition = function(token) {
+  if (this.isEof()) {
+    throw new Error('Scanner reached EOF');
+  }
+
+  var position = jssip.util.Scanner.EOF;
+
+  for (var i = this.index_; i < this.text_.length; i++) {
+    var cursor = 0;
+    while (cursor < token.length && token[cursor] == this.text_[i + cursor]) {
+      cursor++;
+    }
+    if (cursor == token.length) {
+      position = i;
+      this.index_ = position + token.length;
+      break;
+    }
+  }
+
+  if (position == jssip.util.Scanner.EOF) {
+    this.index_ = jssip.util.Scanner.EOF;
+  }
+
+  return position;
+};
