6201cb51212d2bd095d6859e0817ae44659ad7f3
Erick Johnson <erick@ejjohnson.org>
2013-01-12 18:01:03 -0500

    added jssip.message.Response and Request

---
+++ src/message/request.js
@@ -0,0 +1,36 @@
+goog.provide('jssip.message.Request');
+
+goog.require('jssip.message.Message');
+
+
+
+/**
+ * See RFC 3261 Section 7.1 for a description of SIP Requests.
+ * @param {string} method The request method.
+ * @param {string} requestUri The request URI.
+ * @param {string} sipVersion The SIP version string.
+ * @constructor
+ * @extends {jssip.message.Message}
+ */
+jssip.message.Request = function(method, requestUri, sipVersion) {
+  goog.base(this);
+
+  /**
+   * @type {string}
+   * @private
+   */
+  this.method_ = method;
+
+  /**
+   * @type {string}
+   * @private
+   */
+  this.requestUri_ = requestUri;
+
+  /**
+   * @type {string}
+   * @private
+   */
+  this.sipVersion_ = sipVersion;
+};
+goog.inherits(jssip.message.Response, jssip.message.Message);
