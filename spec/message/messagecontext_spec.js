a215d90a44c1e9d5f726dac1ef49e1a410638a64
Erick Johnson <erick@ejjohnson.org>
2013-01-14 01:45:04 -0500

    added beginnings of jssip.message.MessageContext and specs

---
+++ spec/message/messagecontext_spec.js
@@ -0,0 +1,60 @@
+goog.provide('jssip.message.MessageContextSpec');
+
+goog.require('jssip.ParserRegistry');
+goog.require('jssip.message.MessageContext');
+goog.require('jssip.message.MessageParserFactory');
+
+
+describe('jssip.message.MessageContext', function() {
+  var context;
+  var parserRegistry;
+  var requestText = "INVITE sip:bob@biloxi.com SIP/2.0\r\n" +
+    "Foo: request-foo\r\n" +
+    "\r\n";
+  var responseText = "SIP/2.0 200 OK\r\n" +
+    "Foo: response-foo-1\r\n" +
+    "\r\n";
+  var createMessageContext;
+
+  beforeEach(function() {
+    this.addMatchers({
+      toBeRequest: function() {
+        return this.actual instanceof jssip.message.Request;
+      },
+      toBeResponse: function() {
+        return this.actual instanceof jssip.message.Response;
+      }
+    });
+
+    parserRegistry = new jssip.ParserRegistry(
+        new jssip.message.MessageParserFactory());
+    createMessageContext = function(text) {
+      return new jssip.message.MessageContext(text, parserRegistry);
+    }
+  });
+
+  describe('request parsing', function() {
+    beforeEach(function() {
+      context = createMessageContext(requestText);
+
+    });
+
+    it('should create a request message', function() {
+      context.init();
+      expect(context.isRequest()).toBe(true);
+      expect(context.getRequest()).toBeRequest();
+    });
+  });
+
+  describe('response parsing', function() {
+    beforeEach(function() {
+      context = createMessageContext(responseText);
+    });
+
+    it('should create a response message', function() {
+      context.init();
+      expect(context.isRequest()).toBe(false);
+      expect(context.getResponse()).toBeResponse();
+    });
+  });
+});
--- src/message/messagecontext.js
+++ src/message/messagecontext.js
@@ -1,33 +1,105 @@
 goog.provide('jssip.message.MessageContext');
+goog.provide('jssip.message.MessageContext.ParseEvent');
+
+goog.require('goog.events.EventTarget');
+
 
 
 /**
- * A container for passing messages through the SIP stack.
+ * A container for passing messages through the SIP stack and
+ * accessing header and URI values. This is the primary interface for
+ * interacting with messages in the system.  All access to values
+ * present in a message should be requested through this interface.
+ *
  * @param {string} rawMessageText The raw message text.
- * @param {!jssip.message.Message} message The message object.
- * @param {!jssip.Parser} parser The parser instance to be used to handle
- *     further message, header, and URI parsing.
+ * @param {!jssip.ParserRegistry} parserRegistry The parser registry for
+ *     message, header, and URI parsers.
  * @constructor
+ * @extends {goog.events.EventTarget}
  */
-jssip.message.MessageContext = function(rawMessageText, message, parser) {
+jssip.message.MessageContext = function(rawMessageText, parserRegistry) {
+  goog.base(this);
+
   /**
    * @type {string}
    * @private
    */
-  this.rawMessageText_ = messageText;
+  this.rawMessageText_ = rawMessageText;
 
   /**
-   * @type {!jssip.message.Message}
+   * @type {!jssip.ParserRegistry}
    * @private
    */
-  this.message_ = message;
+  this.parserRegistry_ = parserRegistry;
 
   /**
-   * @type {!jssip.Parser}
+   * @type {!Array<!jssip.ParseWarning>}
    * @private
    */
-  this.parser_ = parser;
+  this.parseWarnings_ = [];
+
+  /**
+   * @type {!Array.<!jssip.ParseError>}
+   * @private
+   */
+  this.parseErrors_ = [];
+
+  /**
+   * @type {jssip.message.Message}
+   * @private
+   */
+  this.message_ = null;
+
+  /**
+   * @type {boolean}
+   * @private
+   */
+  this.inited_ = false;
 };
+goog.inherits(jssip.message.MessageContext, goog.events.EventTarget);
+
+
+/**
+ * @enum {string}
+ */
+jssip.message.MessageContext.PARSE_EVENT = {
+  ERROR: 'error',
+  WARNING: 'warning'
+};
+
+
+/**
+ * Initialize the message context, this invokes parsing the raw
+ * message text into a message.  Before initialization listeners may
+ * be set up to handle parse warning and error events emitted from the
+ * message context.
+ */
+jssip.message.MessageContext.prototype.init = function() {
+  this.inited_ = true;
+  this.message_ = this.parseMessage_(this.rawMessageText_);
+};
+
+
+/**
+ * Gets the parse errors in the specified range.
+ * @param {number} start The start index.
+ * @param {number} len The number of errors to retrieve.
+ * @return {!Array.<!jssip.ParseError>} The parse errors.
+ */
+jssip.message.MessageContext.prototype.getParseErrors = function(start, len) {
+  return this.parseErrors_.slice(start, start + len);
+}
+
+
+/**
+ * Gets the parse warnings in the specified range.
+ * @param {number} start The start index.
+ * @param {number} len The number of warnings to retrieve.
+ * @return {!Array.<!jssip.ParseWarning>} The parse warnings.
+ */
+jssip.message.MessageContext.prototype.getParseWarnings = function(start, len) {
+  return this.parseWarnings_.slice(start, start + len);
+}
 
 
 /**
@@ -36,4 +108,135 @@ jssip.message.MessageContext = function(rawMessageText, message, parser) {
  */
 jssip.message.MessageContext.prototype.getRawMessageText = function() {
   return this.rawMessageText_;
+};
+
+
+// TODO(erick): Either clone the message or make it immutable.
+/**
+ * Returns the message if it has been parsed or throws an error.
+ * @return {!jssip.message.Message} The message object.
+ * @throws
+ */
+jssip.message.MessageContext.prototype.getMessage = function() {
+  if (!this.inited_) {
+    throw new Error('Unable to return message on uninitialized context');
+  } else if (!this.message_) {
+    throw new Error('Context initialized but message parsing failed');
+  }
+  return this.message_;
+}
+
+
+/**
+ * @return {boolean} Whether the message is a request. True indicates
+ *     a request, false indicates a response.
+ * @throws
+ */
+jssip.message.MessageContext.prototype.isRequest = function() {
+  return this.getMessage().isRequest();
+};
+
+
+/**
+ * Returns the message coerced to a request if it is such. Throws if
+ * the message is not a request.
+ * @return {!jssip.message.Request} The request.
+ * @throws
+ */
+jssip.message.MessageContext.prototype.getRequest = function() {
+  if (!this.isRequest()) {
+    throw new Error('Message is not a request');
+  }
+  return /** @type {!jssip.message.Request} */ (this.getMessage());
+}
+
+
+/**
+ * Returns the message coerced to a response if it is such. Throws if
+ * the message is not a response.
+ * @return {!jssip.message.Response} The response.
+ * @throws
+ */
+jssip.message.MessageContext.prototype.getResponse = function() {
+  if (this.isRequest()) {
+    throw new Error('Message is not a response');
+  }
+  return /** @type {!jssip.message.Response} */ (this.getMessage());
+}
+
+
+/**
+ * Parses a message object.
+ * @param {string} text The raw message text.
+ * @return {jssip.message.Message} The parsed message or null on error.
+ * @private
+ */
+jssip.message.MessageContext.prototype.parseMessage_ = function(text) {
+  var message = this.invokeParser_(
+      this.parserRegistry_.createMessageParser(text));
+  return /* @type {jssip.message.Message} */ (message);
+};
+
+
+/**
+ * Runs a parser instance's parse method, guarding against parse
+ * errors and collects parse warnings.
+ * @param {!jssip.AbstractParser} parser The parser to invoke.
+ * @return {Object} The parsed object or null if an error was encountered.
+ * @private
+ */
+jssip.message.MessageContext.prototype.invokeParser_ = function(parser) {
+  var result;
+  try {
+    result = parser.parse();
+  } catch (e) {
+    result = null
+    if (e instanceof jssip.ParseError) {
+      this.parseErrors_.push(e);
+      this.dispatchEvent(new jssip.message.MessageContext.ParseEvent(
+          jssip.message.MessageContext.PARSE_EVENT.ERROR,
+          this.parseErrors_.length - 1,
+          1));
+    } else {
+      throw e;
+    }
+  }
+
+  if (parser.parseWarnings.length) {
+    var start = this.parseWarnings_.length;
+    this.parseWarnings_.concat(parser.parseWarnings);
+    this.dispatchEvent(new jssip.message.MessageContext.ParseEvent(
+        jssip.message.MessageContext.PARSE_EVENT.WARNING,
+        start,
+        parser.parseWarnings.length));
+  }
+
+  return result;
+};
+
+
+
+/**
+ * An event describing an anomolous parse event.
+ * @param {jssip.message.MessageContext.PARSE_EVENT} type The event type.
+ * @param {number} start The id of the error or warning.
+ * @param {number} length The number of error or warning events that
+ *     occurred.
+ * @constructor
+ */
+jssip.message.MessageContext.ParseEvent = function(type, start, length) {
+  /**
+   * @type {string}
+   */
+  this.type = type;
+
+  /**
+   * @type {number}
+   */
+  this.start = start;
+
+  /**
+   * @type {number}
+   */
+  this.length = length;
 };
