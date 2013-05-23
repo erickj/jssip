1b4bd188a84e0b494ece0bb8e696ba13686da1a8
Erick Johnson <erick@ejjohnson.org>
2013-05-23 00:13:11 -0400

    event bus accepts an optional parentEventBus parameter for setting up is parent event target

--- src/core/eventbus.js
+++ src/core/eventbus.js
@@ -5,10 +5,16 @@ goog.require('goog.events.EventTarget');
 
 
 /**
+ * @param {!jssip.core.EventBus=} opt_parentEventBus An optional parent event
+ *     bus.  If provided it will be set as this parent of this bus.
  * @constructor
  * @extends {goog.events.EventTarget}
  */
-jssip.core.EventBus = function() {
+jssip.core.EventBus = function(opt_parentEventBus) {
   goog.base(this);
+
+  if (opt_parentEventBus) {
+    this.setParentEventTarget(opt_parentEventBus);
+  }
 };
 goog.inherits(jssip.core.EventBus, goog.events.EventTarget);
