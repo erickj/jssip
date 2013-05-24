91834260ae6ab78f0dc8f0d00e43334dd1af7d9a
Erick Johnson <erick@ejjohnson.org>
2013-05-23 23:43:23 -0400

    moved uriparserfactory -> uriparser

--- src/uri/uriparser.js
+++ src/uri/uriparser.js
@@ -1,6 +1,22 @@
 goog.provide('jssip.uri.UriParser');
+goog.provide('jssip.uri.UriParserFactory');
 
 goog.require('jssip.Parser');
+
+
+
+/**
+ * @interface
+ */
+jssip.uri.UriParserFactory = function() {};
+
+
+/**
+ * Create a new Uri parser for the given Uri.
+ * @param {string} uri
+ * @return {!jssip.uri.UriParser} The uri parser.
+ */
+jssip.uri.UriParserFactory.prototype.createParser = function(uri) {};
 
 
 
