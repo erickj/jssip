4baf0cd2344c78fc5642661dcc2bc17b46ea9a1f
Erick Johnson <erick@ejjohnson.org>
2013-06-07 00:18:25 -0400

    removed record types from src/net/resolver

--- src/net/resolver.js
+++ src/net/resolver.js
@@ -8,15 +8,6 @@ goog.provide('jssip.net.Resolver');
 jssip.net.Resolver = function() {};
 
 
-/** @enum {string} */
-jssip.net.Resolver.ResourceRecordType = {
-  A: 'A',
-  AAAA: 'AAAA',
-  SRV: 'SRV',
-  NAPTR: 'NAPTR'
-}
-
-
 /**
  * @param {string} domain
  * @param {jssip.net.Resolver.ResourceRecordType} rrtype
