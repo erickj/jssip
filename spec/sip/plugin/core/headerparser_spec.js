27f17dd2d7715e0acc116d76cf06a829e8b46ec5
Erick Johnson <erick@ejjohnson.org>
2013-06-09 00:19:25 -0400

    implemented jssip.sip.plugin.core.HeaderParser, plus specs

---
+++ spec/sip/plugin/core/headerparser_spec.js
@@ -0,0 +1,25 @@
+goog.provide('jssip.sip.plugin.core.HeaderParserSpec');
+
+goog.require('jssip.message.Header');
+goog.require('jssip.sip.plugin.core.HeaderParser');
+
+describe('jssip.sip.plugin.core.HeaderParser', function() {
+  var fromHeaderParser;
+  var fromHeaderName = 'From';
+  var fromHeaderValue = '"Erick" <sip:erick@ejjohnson.org>;tag=fd0db81c';
+  var headerPropName = jssip.message.Header.PropertyName;
+
+  beforeEach(function() {
+    fromHeaderParser = new jssip.sip.plugin.core.HeaderParser(
+        fromHeaderName, fromHeaderValue);
+  });
+
+  it('should create a new header', function() {
+    var header = fromHeaderParser.parse();
+    expect(header).toEqual(jasmine.any(jssip.message.Header));
+    expect(header.get(headerPropName.NAME)).toBe('From');
+    expect(header.get(headerPropName.RAW_VALUE)).toBe(fromHeaderValue);
+    expect(header.get(headerPropName.PARSED_VALUE)).toEqual(
+        jasmine.any(Array));
+  });
+});
--- src/sip/plugin/core/headerparser.js
+++ src/sip/plugin/core/headerparser.js
@@ -1,33 +1,79 @@
 goog.provide('jssip.sip.plugin.core.HeaderParser');
 goog.provide('jssip.sip.plugin.core.HeaderParserFactoryImpl');
 
+goog.require('jssip.message.Header');
+goog.require('jssip.message.Header.Builder');
+goog.require('jssip.message.HeaderParser');
+goog.require('jssip.message.HeaderParserFactory');
+goog.require('jssip.parser.AbstractParserFactory');
+goog.require('jssip.parser.ParseError');
+goog.require('jssip.sip.grammar.rfc3261');
+
 
 
 /**
+ * @param {!jssip.event.EventBus} eventBus
  * @constructor
  * @implements {jssip.message.HeaderParserFactory}
+ * @extends {jssip.parser.AbstractParserFactory}
  */
-jssip.sip.plugin.core.HeaderParserFactoryImpl = function() {};
+jssip.sip.plugin.core.HeaderParserFactoryImpl = function(eventBus) {
+  goog.base(this, eventBus);
+};
+goog.inherits(jssip.sip.plugin.core.HeaderParserFactoryImpl,
+    jssip.parser.AbstractParserFactory);
 
 
-/** @override */
-jssip.sip.plugin.core.HeaderParserFactory.prototype.create = function() {
+/**
+ * @override
+ * @return {!jssip.message.HeaderParser}
+ */
+jssip.sip.plugin.core.HeaderParserFactoryImpl.prototype.createParser =
+    function(headerName, headerValue) {
+  var parser = new jssip.sip.plugin.core.HeaderParser(headerName, headerValue);
+  this.setupParser(parser);
+  return parser;
+};
+
+
+
+/**
+ * @param {string} headerName
+ * @param {string} headerValue
+ * @constructor
+ * @implements {jssip.message.HeaderParser}
+ * @extends {jssip.parser.AbstractParser}
+ */
+jssip.sip.plugin.core.HeaderParser = function(headerName, headerValue) {
+  /** @private {string} */
+  this.headerName_ = headerName;
+
+  /** @private {string} */
+  this.headerValue_ = headerValue;
+
+  /** @private {!jssip.message.Header.Builder} */
+  this.builder_ = new jssip.message.Header.Builder();
 };
 
 
 /**
- * @enum {string}
+ * @override
+ * @return {!jssip.message.Header}
  */
-jssip.sip.plugin.core.HeaderParser.HEADERS = {
-  CALL_ID: 'call-id',
-  CONTACT: 'contact',
-  CONTENT_LENGTH: 'content-length',
-  CONTENT_TYPE: 'content-type',
-  CSEQ: 'cseq',
-  FROM: 'from',
-  MAX_FORWARDS: 'max-forwards',
-  TO: 'to',
-  RECORD_ROUTE: 'record-route',
-  ROUTE: 'route',
-  VIA: 'via'
+jssip.sip.plugin.core.HeaderParser.prototype.parse = function() {
+  try {
+    var result =
+      jssip.sip.grammar.rfc3261.parse(this.headerValue_, this.headerName_);
+  } catch (e) {
+    if (e instanceof jssip.sip.grammar.rfc3261.SyntaxError) {
+      throw new jssip.parser.ParseError(e.message);
+    }
+    throw e;
+  }
+  return this.builder_.addPropertyPair(
+        jssip.message.Header.PropertyName.NAME, this.headerName_).
+    addPropertyPair(
+        jssip.message.Header.PropertyName.RAW_VALUE, this.headerValue_).
+    addPropertyPair(
+        jssip.message.Header.PropertyName.PARSED_VALUE, result).build();
 };
