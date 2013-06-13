3313b1678e9caaa1fdba78f5789554a6bf5c833c
Erick Johnson <erick@ejjohnson.org>
2013-06-13 12:37:14 -0400

    some cleanup for TransportLayerFeature, code is incomplete with no specs

--- src/sip/plugin/transport/transportlayerfeature.js
+++ src/sip/plugin/transport/transportlayerfeature.js
@@ -1,25 +1,42 @@
 goog.provide('jssip.sip.plugin.transport.TransportLayerFeature');
 goog.provide('jssip.sip.plugin.transport.TransportLayerFeature.Facade_');
 
+goog.require('goog.async.Deferred');
+goog.require('goog.net.IpAddress');
+goog.require('jssip.async.Promise');
+goog.require('jssip.net.Socket');
+goog.require('jssip.plugin.AbstractFeature');
+goog.require('jssip.plugin.FeatureFacade');
 goog.require('jssip.sip.UserAgent');
+goog.require('jssip.sip.protocol.LookupResult');
 goog.require('jssip.sip.protocol.TransportLayer');
+goog.require('jssip.sip.protocol.rfc3261');
+goog.require('jssip.uri.Uri');
+
 
 
 
 /**
  * @param {string} name
  * @param {!jssip.net.SocketFactoryRegistry} socketFactoryRegistry
+ * @param {!jssip.net.Resolver} resolver
  * @constructor
- * @implements {jssip.sip.protocol.TransportLayer}
+ * @extends {jssip.plugin.AbstractFeature}
  */
 jssip.sip.plugin.transport.TransportLayerFeature =
-    function(name, socketFactoryRegistry) {
+    function(name, socketFactoryRegistry, resolver) {
   /** @private {!jssip.net.SocketFactoryRegistry} */
   this.socketFactoryRegistry_ = socketFactoryRegistry;
+
+  /** @private {!jssip.net.Resolver} */
+  this.resolver_ = resolver;
 
   /** @private {!jssip.sip.plugin.transport.TransportLayerFeature.Facade_} */
   this.facade_ =
       new jssip.sip.plugin.transport.TransportLayerFeature.Facade_(this);
+
+  /** @private {!jssip.event.EventBus} */
+  this.socketEventBus_ = new jssip.event.EventBus();
 
   var featureTypes = [
     jssip.sip.UserAgent.CoreFeatureType.TRANSPORTLAYER
@@ -27,16 +44,110 @@ jssip.sip.plugin.transport.TransportLayerFeature =
   goog.base(this, name, this.facade_, undefined /* opt_eventHandlerMap */,
       featureTypes);
 };
+goog.inherits(
+    jssip.sip.plugin.transport.TransportLayerFeature, jssip.plugin.AbstractFeature);
+
+
+/** @enum {number} */
+jssip.sip.plugin.transport.TransportLayerFeature.DefaultPort = {
+  UDP: 5060,
+  TCP: 5060,
+  SECURE_UDP: 5061,
+  SECURE_TCP: 5061
+};
 
 
 /**
- * @param {!jssip.message.MessageContext} messageContext
+ * Sends a SIP message and returns a promise with a boolen indicating the
+ * message was sent successfully (not that a response was received).
+ * @param {!jssip.message.BuilderMessageContext} messageContext
+ * @return {!jssip.async.Promise.<boolean>}
  */
 jssip.sip.plugin.transport.TransportLayerFeature.prototype.send =
     function(messageContext) {
-  var host = this.resolveHostForMessageContext_(messageContext);
-
+  var destinationUri = this.getUriDestinationForMessage_(messageContext);
+  var promiseWithLookupResults =
+      /** @type {!jssip.async.Promise.<!Array.<!jssip.sip.protocol.LookupResult>>} */ (
+          this.locateSipServerForUri_(destinationUri));
+  return promiseWithLookupResults.thenBranch(
+      goog.bind(this.handleLookupResults_, this, messageContext));
 };
+
+
+/**
+ * @param {!jssip.message.BuilderMessageContext} messageContext
+ * @param {!Array.<!jssip.sip.protocol.LookupResult>} lookupResults
+ * @return {boolean}
+ * @private
+ */
+jssip.sip.plugin.transport.TransportLayerFeature.prototype.
+    handleLookupResults_ = function(messageContext, lookupResults) {
+  var lookupResult = lookupResults.shift();
+  if (lookupResult) {
+    var socket = this.socketFactoryRegistry_.createSocket(
+        lookupResult.getSocketType());
+    socket.connect(
+      lookupResult.getIpAddress().toString(), lookupResult.getPort());
+  }
+};
+
+
+/**
+ * RFC 3263 Details the client process for locating SIP servers
+ *
+ * @see http://tools.ietf.org/html/rfc3263#section-4
+ * @param {jssip.uri.Uri} uri
+ * @return {!jssip.async.Promise.<!Array.<!jssip.sip.protocol.LookupResult>>}
+ * @private
+ */
+jssip.sip.plugin.transport.TransportLayerFeature.prototype.
+    locateSipServerForUri_ = function(uri) {
+  var ipAddress = goog.net.IpAddress.fromString(uri.getHost());
+  if (ipAddress) {
+    var port = uri.getPort() || 5060;
+    var protocol = jssip.net.Socket.Type.UDP;
+    return jssip.async.Promise.succeed(
+        [new jssip.sip.protocol.LookupResult(ipAddress, port, protocol)]);
+  }
+  // TODO(erick): lots...
+  return jssip.async.Promise.succeed([
+      new jssip.sip.protocol.LookupResult(
+          new goog.net.Ipv4Address('0.0.0.0'),
+          5060,
+          jssip.net.Socket.Type.UDP)]);
+};
+
+
+/**
+ * The funcationality defined here is defined as part of UAC behavoir in 3261,
+ * but it fits better architecturally here.  Collects a set of destinations to
+ * try to send the message to.
+ *
+ * @see RFC 3261 Section 8.1.2
+
+   If the first element in the route set indicated a strict router (resulting in
+   forming the request as described in Section 12.2.1.1), the procedures MUST be
+   applied to the Request-URI of the request.  Otherwise, the procedures are
+   applied to the first Route header field value in the request (if one exists),
+   or to the request's Request-URI if there is no Route header field present.
+
+ * @param {!jssip.message.BuilderMessageContext} messageContext
+ * @return {!jssip.uri.Uri}
+ */
+jssip.sip.plugin.transport.TransportLayerFeature.prototype.
+    getUriDestinationForMessage_ = function(messageContext) {
+  var routeSet = messageContext.getRouteSet();
+  if (!routeSet.isEmpty() && !routeSet.get(0).isLooseRouter()) {
+    return messageContext.getMessage().getRequestUri();
+  }
+  var routes = jssip.sip.protocol.Routes.createFromHeader(
+      messageContext.getHeader(jssip.sip.protocol.rfc3261.HeaderType.ROUTE));
+  if (routes.length > 0) {
+    return routes[0].getUri();
+  }
+  return messageContext.getMessage().getRequestUri();
+};
+
 
 
 
@@ -44,6 +155,7 @@ jssip.sip.plugin.transport.TransportLayerFeature.prototype.send =
  * @param {!jssip.sip.plugin.transport.TransportLayerFeature} delegate
  * @constructor
  * @private
+ * @implements {jssip.plugin.FeatureFacade}
  * @implements {jssip.sip.protocol.TransportLayer}
  */
 jssip.sip.plugin.transport.TransportLayerFeature.Facade_ = function(delegate) {
@@ -55,5 +167,5 @@ jssip.sip.plugin.transport.TransportLayerFeature.Facade_ = function(delegate) {
 /** @override */
 jssip.sip.plugin.transport.TransportLayerFeature.Facade_.prototype.send =
     function(messageContext) {
-  this.delegate_.send(messageContext);
+  return this.delegate_.send(messageContext);
 };
