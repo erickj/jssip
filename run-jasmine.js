a1fda1bfd19cf2735fd4521a5338e3615aa56419
Erick Johnson <erick@ejjohnson.org>
2013-06-15 21:22:04 -0400

    run-jasmine.js exit with nonzero status when specs can't be run

--- run-jasmine.js
+++ run-jasmine.js
@@ -60,8 +60,8 @@ page.onConsoleMessage = function(msg) {
 
 page.open(system.args[1], function(status){
   if (status !== "success") {
-    console.log("Unable to access network");
-    phantom.exit();
+    console.log("Unable to run Specs, exit status:", status);
+    phantom.exit(1);
   } else {
     waitFor(function(){
       return page.evaluate(function(){
